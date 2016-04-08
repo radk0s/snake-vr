d604fc9f99ebf0008a488c191e52de0fec3afced
IwanBugajski <iwan.bugajski@gmail.com>
2016-04-08 18:23:03 +0200

    camera moving

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+    private float speed = 2.0f;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Camera.main.transform.forward * speed * Time.deltaTime);
+    }
+}
