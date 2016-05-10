4518072c1d55ae29fe2ccba55024583e979c0fc6
rchamot <radoslaw.chamot@schibsted.pl>
2016-05-10 07:37:58 +0200

    add losing scene

---
+++ Assets/LosingGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LosingGame : MonoBehaviour {
+
+
+	void Start()
+	{
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Jeep")
+		{
+			Application.LoadLevel("GameOver");
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Jeep")
+		{
+			Application.LoadLevel("GameOver");
+		}
+
+	}
+}
