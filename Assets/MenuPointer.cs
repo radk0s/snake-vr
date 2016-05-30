6a7bf6208781045f1084dcd9de4ff776a7dc6bee
IwanBugajski <iwan.bugajski@gmail.com>
2016-05-30 21:36:40 +0200

    buttons following camera

--- Assets/MenuPointer.cs
+++ Assets/MenuPointer.cs
@@ -27,35 +27,54 @@ public abstract class AbstractButtonBehavior : MonoBehaviour
 {
     private bool selected = false;
     private float selectedAt;
+    private float distanceToCamera;
     private const float selectionTime = 2.5f;
-    private Color selectedColor = Color.yellow;
-    private Color notSelectedColor = Color.white;
+    private Color selectedColor = Color.white;
+    private Color notSelectedColor = new Color(1.0f, 0.5f, 1.0f);
+    private Vector3 initialScale;
+
+    void Start()
+    {
+        distanceToCamera = 2.0f;
+        initialScale = gameObject.transform.localScale;
+    }
 
     void Update()
     {
         //rotate
         Vector3 rotation = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
         gameObject.transform.eulerAngles = rotation;
-        
+        Vector3 position = Camera.main.transform.forward * distanceToCamera;
+        position.y = gameObject.transform.position.y;
+        gameObject.transform.position = position;
 
 
-        if(selected && Time.time - selectedAt > selectionTime)
+        if (selected)
         {
-            PerformAction();
+            float selectionTimeSoFar = Time.time - selectedAt;
+            float scaleY = 1.0f - selectionTimeSoFar / selectionTime;
+            Vector3 currentScale = new Vector3(initialScale.x * (scaleY >= 0 ? scaleY : 0), initialScale.y, initialScale.z);
+            gameObject.transform.localScale = currentScale;
+
+            if (selectionTimeSoFar >= selectionTime)
+            {
+                PerformAction();
+            }
         }
     }
 
     public void Select()
     {
         this.selected = true;
-        this.gameObject.GetComponent<Renderer>().material.color = selectedColor;
+        this.gameObject.GetComponent<SpriteRenderer>().color = selectedColor;
         this.selectedAt = Time.time;
     }
 
     public void Deselect()
     {
         this.selected = false;
-        this.gameObject.GetComponent<Renderer>().material.color = notSelectedColor;
+        this.gameObject.GetComponent<SpriteRenderer>().color = notSelectedColor;
+        this.gameObject.transform.localScale = initialScale;
     }
 
     public abstract void PerformAction();
